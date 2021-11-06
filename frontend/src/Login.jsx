e4249ffac86dc03dbda3e8f3cb08993c4c754191
Sree Grandhe <sreegrandhe@live.com>
2021-11-06 12:32:04 -0500

    Changed redirect url on success

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -11,7 +11,7 @@ export class Login extends React.Component {
             // Popup signin flow rather than redirect flow.
             signInFlow: 'popup',
             // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
-            signInSuccessUrl: '/',
+            signInSuccessUrl: '/projects',
             // We will display Google and Facebook as auth providers.
             signInOptions: [
                 firebase.auth.GoogleAuthProvider.PROVIDER_ID,
