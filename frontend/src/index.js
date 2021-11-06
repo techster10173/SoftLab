45e1f942497b9e124882e3850a18f2a15de79cf9
Sree Grandhe <sreegrandhe@live.com>
2021-11-06 12:44:51 -0500

    Add Material UI Library

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -19,6 +19,10 @@ firebase.initializeApp(config);
 
 ReactDOM.render(
   <React.StrictMode>
+    <link
+      rel="stylesheet"
+      href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+    />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
