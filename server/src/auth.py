ef1f43e49001a890605f1b2989cca21b0ada8a4e
Sree Grandhe <sreegrandhe@live.com>
2021-11-07 11:32:41 -0600

    Define firebase credentials for authentication middleware

--- server/src/auth.py
+++ server/src/auth.py
@@ -1,19 +1,18 @@
 import firebase_admin
-from flask import request
+from flask import request, jsonify
 from functools import wraps
 
-cred = firebase_admin.credentials.Certificate('../softlab-6e579-firebase-adminsdk-ms1dc-03c667f3d7.json')
-firebase = firebase_admin.initialize_app(cred)
+firebase = firebase_admin.initialize_app()
 
 def check_token(f):
     @wraps(f)
     def wrap(*args,**kwargs):
         if not request.headers.get('authorization'):
-            return {'message': 'No token provided'},400
+            return jsonify({'message': 'No token provided'}), 400
         try:
             user = firebase_admin.auth.verify_id_token(request.headers['authorization'])
-            request.user = user
+            request.user = user.uid
         except:
-            return {'message':'Invalid token provided.'},400
+            return jsonify({'message':'Invalid token provided.'}), 400
         return f(*args, **kwargs)
     return wrap
