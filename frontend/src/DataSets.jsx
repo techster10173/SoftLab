d93bda8778c8a7b53c0cf81d9fed2db19ef8cd3a
Sree Grandhe <sreegrandhe@live.com>
2021-11-08 17:49:56 -0600

    Restyle Datasets

--- frontend/src/DataSets.jsx
+++ frontend/src/DataSets.jsx
@@ -1,32 +1,22 @@
 import {Navbar} from "./Toolbar.jsx";
 import React from "react";
 import './projects.css';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import Box from '@mui/material/Box';
+import {TableBody, TableCell, TableRow, TableContainer, TableHead, Paper, Box, Table} from '@mui/material';
 import data from "./Data.json"
 
 export class DataSets extends React.Component {
     render() {
         return (
-            <div>
+            <>
                 <Navbar />
-          <Box
-              sx={{
-                marginLeft: 10,
-                marginRight: 10,
-                marginTop: 10,
-                marginBottom: 10,
-                
-                boxShadow: '0 0 1px 3px rgba(0, 0, 0, .125)',
-            }}
-          >
-            <TableContainer component={Paper}>
+                <TableContainer component={Paper} sx={{
+                    marginLeft: 5,
+                    marginRight: 5,
+                    marginTop: 5,
+                    width: "auto",
+                    marginBottom: 5,
+                    boxShadow: '0 0 1px 3px rgba(0, 0, 0, .125)',
+                }}>
               <Table aria-label="simple table">
                 <TableHead>
                   <TableRow>
@@ -53,9 +43,7 @@ export class DataSets extends React.Component {
                 </TableBody>
               </Table>
             </TableContainer>
-            
-            </Box>
-        </div>
+          </>
            
         )
     }
