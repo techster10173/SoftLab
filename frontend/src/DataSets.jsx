667bfc06e3762aaaf376fbcf282bdee25b0c2996
Saiteja <srawulwar@utexas.edu>
2021-11-08 14:19:38 -0600

    dataset testing

--- frontend/src/DataSets.jsx
+++ frontend/src/DataSets.jsx
@@ -17,10 +17,26 @@ import Box from '@mui/material/Box';
     }
     
     const rows = [
-      createData('Project 1', 'testing', '30mb',<a href='/somefile.txt' download>Click to download</a>),
-      createData('Project 2', 'testing', '30mb',<a href='/somefile.txt' download>Click to download</a>),
-      createData('Project 3', 'testing', '30mb',<a href='/somefile.txt' download>Click to download</a>),
-      
+      createData('Abdominal and Direct Fetal ECG Database', 'The research material included in the Abdominal and Direct Fetal Electrocardiogram Database contains multichannel fetal electrocardiogram (FECG) recordings obtained from 5 different women in labor, between 38 and 41 weeks of gestation. The recordings were acquired in the Department of Obstetrics at the Medical University of Silesia, by means of the KOMPOREL system for acquisition and analysis of fetal electrocardiogram (ITAM Institute, Zabrze, Poland). Each recording comprises four differential signals acquired from maternal abdomen and the reference direct fetal electrocardiogram registered from the fetal head.', '14.6 MB',<a href='https://physionet.org/static/published-projects/adfecgdb/abdominal-and-direct-fetal-ecg-database-1.0.0.zip' download>Click to download</a>),
+      createData('AF Termination Challenge Database', 'This database of two-channel ECG recordings has been created for use in the Computers in Cardiology Challenge 2004, an open competition with the goal of developing automated methods for predicting spontaneous termination of atrial fibrillation (AF).', '2.4 MB',<a href='https://physionet.org/static/published-projects/aftdb/af-termination-challenge-database-1.0.0.zip' download>Click to download</a>),
+      createData('ANSI/AAMI EC13 Test Waveforms', 'The files in this set can be used for testing a variety of devices that monitor the electrocardiogram. The recordings include both synthetic and real waveforms.', '1.1 MB',<a href='https://physionet.org/static/published-projects/aami-ec13/ansiaami-ec13-test-waveforms-1.0.0.zip' download>Click to download</a>),
+      createData('Project 3', 'testing', '30 mb',<a href='/somefile.txt' download>Click to download</a>),
+      createData('Project 3', 'testing', '30 mb',<a href='/somefile.txt' download>Click to download</a>),
+      createData('Project 3', 'testing', '30 mb',<a href='/somefile.txt' download>Click to download</a>),
+      createData('Project 3', 'testing', '30 mb',<a href='/somefile.txt' download>Click to download</a>),
+      createData('Project 3', 'testing', '30 mb',<a href='/somefile.txt' download>Click to download</a>),
+      createData('Project 3', 'testing', '30 mb',<a href='/somefile.txt' download>Click to download</a>),
+      createData('Project 3', 'testing', '30 mb',<a href='/somefile.txt' download>Click to download</a>),
+      createData('Project 3', 'testing', '30 mb',<a href='/somefile.txt' download>Click to download</a>),
+      createData('Project 3', 'testing', '30 mb',<a href='/somefile.txt' download>Click to download</a>),
+      createData('Project 3', 'testing', '30 mb',<a href='/somefile.txt' download>Click to download</a>),
+      createData('Project 3', 'testing', '30 mb',<a href='/somefile.txt' download>Click to download</a>),
+      createData('Project 3', 'testing', '30 mb',<a href='/somefile.txt' download>Click to download</a>),
+      createData('Project 3', 'testing', '30 mb',<a href='/somefile.txt' download>Click to download</a>),
+      createData('Project 3', 'testing', '30 mb',<a href='/somefile.txt' download>Click to download</a>),
+      createData('Project 3', 'testing', '30 mb',<a href='/somefile.txt' download>Click to download</a>),
+      createData('Project 3', 'testing', '30 mb',<a href='/somefile.txt' download>Click to download</a>),
+      createData('Project 3', 'testing', '30 mb',<a href='/somefile.txt' download>Click to download</a>),
     ];
 
 export class DataSets extends React.Component {
