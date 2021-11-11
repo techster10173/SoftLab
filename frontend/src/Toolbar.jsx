fc7c9124c4d732f62c483dd1d9d178a90ac72b8a
Sree Grandhe <sreegrandhe@live.com>
2021-11-10 22:40:31 -0600

    Implement logout functionality

--- frontend/src/Toolbar.jsx
+++ frontend/src/Toolbar.jsx
@@ -1,17 +1,30 @@
 import {Toolbar, AppBar} from '@material-ui/core';
-import {Link} from "react-router-dom";
+import axios from 'axios';
+import {Link, Navigate} from "react-router-dom";
 import logo from "./assets/BB.png"
 import './home.css';
+import { useState } from 'react';
 
 export function Navbar(props){
+    const [open, setOpen] = useState(false);
+
+    const logout = () => {
+        axios.post('/api/auth/signout').then(res => {
+            setOpen(true);
+        }).catch(err => {
+            console.error(err);
+        });
+    }
+
     return(
+        <>
             <AppBar color="primary" position="relative">
                 <Toolbar>
                     <div>
-                        <img src={logo} alt="logo" class="img1"/>
+                        <img src={logo} alt="logo" className="img1"/>
                     </div>
 
-                    <div class= "menu">
+                    <div className="menu">
                         <ul>
                             <li>
                                 <Link to="/projects">Projects</Link>
@@ -23,11 +36,13 @@ export function Navbar(props){
                                 <Link to="/data">Data Sets</Link>
                             </li>
                             <li>
-                                <Link to="/">Logout</Link>
+                                <a href="#" onClick={logout}>Logout</a>
                             </li>
                         </ul>
                     </div>
                 </Toolbar>
             </AppBar>
+            {open ? <Navigate to="/"/> : null}
+            </>
     )
 }
